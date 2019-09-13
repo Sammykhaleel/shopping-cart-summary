5057d62962f3b52388075f7aebeadf7abf507673
sammykhaleel <sammykhaleel83@gmail.com>
2019-09-13 01:03:45 -0400

    add functionality to show and hid an ItemDetails with collapse

--- src/components/ItemDetails/ItemDetails.js
+++ src/components/ItemDetails/ItemDetails.js
@@ -19,6 +19,22 @@ export default class ItemDetails extends Component {
           {this.state.open === false ? `See` : `Hide`} item details
           {this.state.open === false ? `+` : `-`}
         </Button>
+        <Collapse in={this.state.open}>
+          <div>
+            <Well>
+              <Media>
+                <Media.Left>
+                  <img
+                    width={100}
+                    height={100}
+                    alt="thumbnail"
+                    src="https://i5.walmartimages.com/asr/58f40ef2-41ef-4290-b393-920f63da60ba_1.2b51c944c164d2a5e242e628dd402a9b.jpeg?odnWidth=undefined&odnHeight=undefined&odnBg=ffffff"
+                  />
+                </Media.Left>
+              </Media>
+            </Well>
+          </div>
+        </Collapse>
       </div>
     );
   }
